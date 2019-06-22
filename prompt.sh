45a7339e7440d323967b0787e0490a1ef173fa54
Parity Account <apar@adaq3.jlab.org>
2019-06-21 22:03:11 -0400

    Updated shell scripts

--- prompt.sh
+++ prompt.sh
@@ -5,31 +5,27 @@ runnum=$1;
 timenow=$(date +"%Y-%m%d-%H%M");
 
 ./qwparity -r $runnum -c prex_prompt.conf \
-    --enable-lrbcorrection \
-    --enable-correlator \
-    --enable-correction \
     --rootfile-stem prexPrompt_pass1_ \
     --QwLog.logfile ./LogFiles/QwLog_run$runnum\_prompt_pass1_$timenow.txt ;
 
-cp ./japanOutput/summary_$runnum.txt \
-    ./SummaryText/summary_prompt_pass1_$runnum.txt;
+# cp ./japanOutput/summary_$runnum.txt \
+#     ./SummaryText/summary_prompt_pass1_$runnum.txt;
 
 #JAPAN Second Pass
 timenow=$(date +"%Y-%m%d-%H%M");
 
 ./qwparity -r $runnum -c prex_prompt.conf \
-    --enable-lrbcorrection \
-    --enable-correlator \
-    --enable-correction \
     --rootfile-stem prexPrompt_pass2_ \
     --QwLog.logfile ./LogFiles/QwLog_run$runnum\_prompt_pass2_$timenow.txt ;
 
 # *Assumming prompt summary gets updated at 2nd pass.
-cp ./japanOutput/summary_$runnum.txt \
-    ./SummaryText/summary_prompt_$runnum.txt;
+cp ./japanOutput/summary_*$runnum*.txt \
+    ./SummaryText/
 
 # BeamMod Data Extraction (FIXME)
+# BeamMod Instant Slope Calculation (FIXME)
+# BeamMod Instant correction and summary (FIXME)
 
-# Make Summary Plots/Text and Rsyncs to HallA onlineWeb
-#./summary.sh $runnum;
+# Make Summary Plots/Text and sync to HallA onlineWeb
+./summary.sh $runnum;
     
--- summary.sh
+++ summary.sh
@@ -1,21 +1,43 @@
-#! /bin/sh
+#!/bin/sh
 
-runnum=$1;
+runnum=$1
+level="Prompt"
+shopt -s extglob
+# find split file
+rootfile_list=$(ls -1 ./japanOutput/prex$level\_pass2_$runnum.!(*jlab.org*).root);
+shopt -u extglob
 
-# Making Summary Plots
-if [ ! -d ./SummaryPlots/run$runnum ]; then
-    mkdir ./SummaryPlots/run$runnum;
-fi
-root -b -q -l './rootMacros/PlotSummary.C('$runnum')';
+for rootfile  in $rootfile_list
+do
+    # strip out [run.seg]
 
-#Rsync to OnlineWeb folder
-if [ ! -d ./hallaweb_online/summary/run$runnum ]; then
-    mkdir ./hallaweb_online/summary/run$runnum;
-fi
-rsync -a ./SummaryPlots/run$runnum/
-    ./hallaweb_online/summary/run$runnum  ;
+    trim=${rootfile%.root}
+    run_dot_seg=${trim#*pass2_}
 
-#Change user group and permission
-chgrp -R a-parity ./hallaweb_online/summary/run$runnum;
-chmod -R 755 ./hallaweb_online/summary/run$runnum;
-    
+    run_seg=${run_dot_seg/./_}
+    if [ ! -d ./SummaryPlots/run$run_seg ]; then
+	mkdir ./SummaryPlots/run$run_seg;
+    fi
+
+    root -b -q -l './rootMacros/PlotSummary.C("'$rootfile'")';
+
+    if [ ! -d ./hallaweb_online/summary/run$run_seg ]; then
+	mkdir ./hallaweb_online/summary/run$run_seg;
+    fi
+
+    cp  ./SummaryPlots/run$run_seg/* \
+	./hallaweb_online/summary/run$run_seg/;
+
+    mv  ./SummaryText/summary_$run_dot_seg.txt \
+	./SummaryText/summary_$level_$run_seg.txt \
+
+    cp  ./SummaryText/summary_$level_$run_seg.txt \
+	./hallaweb_online/summary/run$run_seg/ ;
+
+    #Change user group and permission
+    chgrp -R a-parity ./hallaweb_online/summary/run$run_seg;
+    chmod -R 755 ./hallaweb_online/summary/run$run_seg;    
+
+done
+
+
