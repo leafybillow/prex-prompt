22e9343e0da40032fff19cf0fa5cfd7baaa05055
Parity Account <apar@adaq3.jlab.org>
2019-06-25 22:12:25 -0400

    backup testing scripts

--- rootMacros/Test.C
+++ rootMacros/Test.C
@@ -1,33 +1,42 @@
+//////////////////////////////////////////////////////////////////////
+//
+// PlotSummary.C (Testing script)
+//   Tao Ye - May 2019
+//
+//////////////////////////////////////////////////////////////////////
+
 #include "device_list.h"
 
+#include "PlotCorrelation.C"
 
-#include "CheckBCMdd.C"
-#include "CheckEventCut.C"
-#include "PlotDeviceEvent.C"
-#include "PlotBlocks.C"
 #include "PlotInjBPMS.C"
 #include "PlotInjBPMSAq.C"
 #include "PlotInjBPMSDr.C"
-#include "PlotCorrelation.C"
+#include "PlotInjBPMSAelli.C"
+
+#include "CheckBCM.C"
+#include "CheckBCMdd.C"
+#include "CheckBPM.C"
+#include "PlotBPMDiffCorrelation.C"
+#include "CheckSAM.C"
+#include "CheckSAMCorrelation.C"
+#include "CheckRegression.C"
+//#include "CheckPairSAM.C"
+#include "CheckComboSAM.C"
 #include "PlotErrorCounters.C"
 #include "Integrated.C"
-#include "CheckBPM.C"
-#include "CheckSAM.C"
-#include "CheckRegression.C"
-#include "CheckPairSAM.C"
 
-void Test(Int_t run_num){
-
-  run_number = run_num;
-  path = "./japanOutput/";
-  prefix = "prexRespin2_pass2";
-  file_name = Form("%s_%d.root",prefix.Data(),run_number);
-  TFile* rootfile = TFile::Open(path+file_name);
+void Test(TString filename){
+  japanOutput = TFile::Open(filename);
+  Ssiz_t pfirst = filename.Last('_')+1;
+  Ssiz_t plast = filename.Last('.')-1;
+  Ssiz_t plength = plast-pfirst+1;
+  run_seg = filename(pfirst,plength);
+  run_seg = run_seg.ReplaceAll('.','_');
   output_path = "./";
 
-  // CheckEventCut();
-  //  PlotDeviceEvent();
-  // CheckBPM();
-  CheckPairSAM();
+  // ===Check mulc_lrb before making plots
+  CheckComboSAM();
+
 }
 
