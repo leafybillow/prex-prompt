af968353e7efef909533c014ed41f76bb6445a9a
Tao Ye <tao.ye@stonybrook.edu>
2019-06-30 12:50:55 -0400

    Added Left and Right arm correlation prof-fit

--- rootMacros/CheckRegressedDetector.C
+++ rootMacros/CheckRegressedDetector.C
@@ -30,6 +30,9 @@ void CheckRegressedDetector(){
   mul_tree->Draw("asym_usl/ppm-asym_usr/ppm","ErrorFlag==0");
   c1->cd(5);
   mul_tree->Draw("asym_usl/ppm+asym_usr/ppm-2*Aq","ErrorFlag==0");
+  c1->cd(6);
+  mul_tree->Draw("asym_usl/ppm-Aq/ppm:asym_usr/ppm-Aq/ppm",
+		 "ErrorFlag==0","prof");
     
   plot_title = Form("run%s_upstreamLR_ratecheck_raw.png",run_seg.Data());
 
@@ -48,15 +51,19 @@ void CheckRegressedDetector(){
   c1->cd(2);
   mul_tree->Draw("asym_dsr/ppm-Aq/ppm","ErrorFlag==0");
   c1->cd(3);
-  mul_tree->Draw("asym_dsl/ppm-Aq:asym_dsr/ppm-Aq/ppm","ErrorFlag==0");
+  mul_tree->Draw("asym_dsl/ppm-Aq/ppm:asym_dsr/ppm-Aq/ppm","ErrorFlag==0");
   c1->cd(4);
   mul_tree->Draw("asym_dsl/ppm-asym_dsr/ppm","ErrorFlag==0");
   c1->cd(5);
   mul_tree->Draw("asym_dsl/ppm+asym_dsr/ppm-2*Aq/ppm","ErrorFlag==0");
-    
+  c1->cd(6);
+  mul_tree->Draw("asym_dsl/ppm-Aq/ppm:asym_dsr/ppm-Aq/ppm","ErrorFlag==0",
+		 "prof");
   plot_title = Form("run%s_downstreamLR_ratecheck_raw.png",run_seg.Data());
 
   // Regression Corrected
+  TH1 *hfit, *h_buff;
+  Double_t low,up;
   // Upstream
   t1 = new TText(0.0,0.007,plot_title);
   t1->SetNDC();
@@ -75,8 +82,27 @@ void CheckRegressedDetector(){
   c1->cd(4);
   mul_tree->Draw("(cor_usl/ppm-cor_usr/ppm)/2","ErrorFlag==0");
   c1->cd(5);
-  mul_tree->Draw("(cor_usl/ppm+cor_usr/ppm)/2-Aq","ErrorFlag==0");
-    
+  mul_tree->Draw("(cor_usl/ppm+cor_usr/ppm)/2-Aq/ppm","ErrorFlag==0");
+  c1->cd(6);
+  mul_tree->Draw("cor_usl/ppm-Aq/ppm:cor_usr/ppm-Aq/ppm>>hcoruslr","ErrorFlag==0",
+		 "prof");
+  gPad->Update();
+  hfit = (TH2D*)gDirectory->FindObject("hcoruslr");
+  TPaveStats *psus = (TPaveStats*)hfit->FindObject("stats");
+  if(psus!=NULL){
+    psus->SetOptFit(1);
+    psus->SetOptStat(0);
+    psus->SetX1NDC(0.0);
+    psus->SetY1NDC(0.95);
+    psus->SetX2NDC(0.35);
+    psus->SetY2NDC(0.7);
+  }
+  if(hfit!=NULL){
+    low = hfit->GetMean()-2*(hfit->GetRMS());
+    up = hfit->GetMean()+2*(hfit->GetRMS());
+    hfit->Fit("pol1","QR","",low,up);
+  } 
+
   plot_title = Form("run%s_upstreamLR_ratecheck_reg.png",run_seg.Data());
 
   t1 = new TText(0.0,0.007,plot_title);
@@ -93,11 +119,31 @@ void CheckRegressedDetector(){
   c1->cd(2);
   mul_tree->Draw("cor_dsr/ppm-Aq/ppm","ErrorFlag==0");
   c1->cd(3);
-  mul_tree->Draw("cor_dsl/ppm-Aq:cor_dsr/ppm-Aq/ppm","ErrorFlag==0");
+  mul_tree->Draw("cor_dsl/ppm-Aq/ppm:cor_dsr/ppm-Aq/ppm","ErrorFlag==0");
   c1->cd(4);
   mul_tree->Draw("(cor_dsl/ppm-cor_dsr/ppm)/2","ErrorFlag==0");
   c1->cd(5);
   mul_tree->Draw("(cor_dsl/ppm+cor_dsr/ppm)/2-Aq/ppm","ErrorFlag==0");
+  c1->cd(6);
+  mul_tree->Draw("cor_dsl/ppm-Aq/ppm:cor_dsr/ppm-Aq/ppm>>hcordslr","ErrorFlag==0",
+		 "prof");
+  gPad->Update();
+  hfit = (TH2D*)gDirectory->FindObject("hcordslr");
+  TPaveStats *psds = (TPaveStats*)hfit->FindObject("stats");
+  if(psds!=NULL){
+    psds->SetOptFit(1);
+    psds->SetOptStat(0);
+    psds->SetX1NDC(0.0);
+    psds->SetY1NDC(0.95);
+    psds->SetX2NDC(0.35);
+    psds->SetY2NDC(0.7);
+  }
+  if(hfit!=NULL){
+    low = hfit->GetMean()-2*hfit->GetRMS();
+    up = hfit->GetMean()+2*hfit->GetRMS();
+    hfit->Fit("pol1","QR","",low,up);
+
+  } 
     
   plot_title = Form("run%s_downstreamLR_ratecheck_reg.png",run_seg.Data());
 
@@ -109,15 +155,17 @@ void CheckRegressedDetector(){
   c1->SaveAs(output_path+plot_title);
   c1->Clear("D");
 
-  //  ======== vSAM vs AqDS
+  //  ======== vMainDet vs AqDS
   gStyle->SetOptFit(1);
   gStyle->SetOptStat(0);
+  gStyle->SetStatW(0.3);
+  gStyle->SetStatH(0.25);
   TCanvas *c0 = new TCanvas("c0","c0",2400,1200);
   c0->Divide(4,2);
   TString filename;
   TText *label;
-  TH1* hfit;
-  Double_t low,up;
+
+
   TString user_cut = "ErrorFlag==0";
   for(int i=0;i<4;i++){
     c0->cd(i+1);
@@ -144,7 +192,7 @@ void CheckRegressedDetector(){
   label->Draw("same");
   c0->SaveAs(output_path+filename);
   c0->Clear("D");
-  //  ======== vSAM vs AqUS
+  //  ======== vMainDet vs AqUS
   for(int i=0;i<4;i++){
     c0->cd(i+1);
     mul_tree->Draw(Form("cor_%s/ppm:AqUS/ppm",vMainDet[i])
