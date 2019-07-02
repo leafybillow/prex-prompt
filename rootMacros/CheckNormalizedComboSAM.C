fafc0158486c9abbb72c39f05ba25495029121a5
Tao Ye <tao.ye@stonybrook.edu>
2019-07-02 13:23:48 -0400

    root scripts for normalized dtectors

---
+++ rootMacros/CheckNormalizedComboSAM.C
@@ -0,0 +1,264 @@
+// CheckComboSAM.C : 
+//	ROOT script for SAM resolution check for japan Ouputs
+//
+//  author: Tao Ye 
+//  	06-2019
+
+#include "device_list.h"
+void CheckComboSAM(vector<TString> vDet, TString tag,TString user_cut);
+
+void CheckComboSAM(){
+
+  vector<TString> vSAMU = {"cor_sam2","cor_sam6","cor_sam4","cor_sam8"};
+  vector<TString> vSAMV = {"cor_sam1","cor_sam5","cor_sam3","cor_sam7"};
+
+  CheckComboSAM(vSAMU,"cor_sam2648","ErrorFlag==0");  // FIXME : It should be device_Error_Code  (Maybe ... )
+  CheckComboSAM(vSAMV,"cor_sam1537","ErrorFlag==0");
+
+}
+void CheckComboSAM(vector<TString> vDet, TString tag, TString user_cut){
+
+  TTree *mul_tree = (TTree*)gROOT ->FindObject("mul");
+  TTree *mulc_tree = (TTree*)gROOT ->FindObject("mulc_lrb");
+  mulc_tree->AddFriend(mul_tree);
+
+  mulc_tree->SetAlias("AqDS", "asym_bcm_an_ds3");
+  mulc_tree->SetAlias("AqUS", "asym_bcm_an_us");
+  mulc_tree->SetAlias("AverageLumi",
+		      Form("(%s+%s+%s+%s)/4",
+			   vDet[0].Data(),vDet[1].Data(),vDet[2].Data(),vDet[3].Data()));
+
+
+  // FIXME : Check if mulc_tree exists or if it is empty
+  // FIXME : Check if DV exists or if it is empty
+  
+  TString filename;
+  TText *label;
+  TH1 *hfit;
+  Double_t low, up;
+
+  // Pairing Detectors 
+
+  gStyle->SetOptStat(1);
+  gStyle->SetOptFit(1);
+  gStyle->SetStatW(0.3);
+  gStyle->SetStatH(0.25);
+  gStyle->SetStatX(0.95);
+  gStyle->SetStatY(0.95);
+
+  TCanvas *c1 = new TCanvas("c1","c1",2400,1600);
+  c1->Divide(3,2);
+  c1->cd(1);
+  mulc_tree->Draw(Form("%s/ppm",vDet[0].Data()),
+		  user_cut);
+  c1->cd(2);
+  mulc_tree->Draw(Form("%s/ppm",vDet[1].Data())
+		  ,user_cut);
+  c1->cd(3);
+  mulc_tree->Draw(Form("%s/ppm : %s/ppm",vDet[0].Data(),vDet[1].Data()),
+		  user_cut);
+  c1->cd(4);
+  mulc_tree->Draw(Form("%s/ppm : %s/ppm",vDet[0].Data(),vDet[1].Data()),
+		  user_cut,"prof");
+
+  c1->cd(5);
+  mulc_tree->Draw(Form("(%s/ppm - %s/ppm)/2",vDet[0].Data(),vDet[1].Data()),
+		  user_cut);
+
+  c1->cd(6);  
+  mulc_tree->Draw(Form("(%s/ppm + %s/ppm)/2",vDet[0].Data(),vDet[1].Data()),
+		  user_cut);
+
+  filename = Form("run%s_combo_%s_vs_%s.png",
+		  run_seg.Data(),
+		  vDet[0].Data(),
+		  vDet[1].Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c1->cd();
+  label->Draw("same");
+  c1->SaveAs(output_path+filename);
+  c1->Clear("D");
+
+  // The other pair
+  c1->cd(1);
+  mulc_tree->Draw(Form("%s/ppm",vDet[2].Data()),
+		  user_cut);
+  c1->cd(2);
+  mulc_tree->Draw(Form("%s/ppm ",vDet[3].Data())
+		  ,user_cut);
+  c1->cd(3);
+  mulc_tree->Draw(Form("%s/ppm  : %s/ppm ",vDet[2].Data(),vDet[3].Data()),
+		  user_cut);
+  c1->cd(4);
+  mulc_tree->Draw(Form("%s/ppm  : %s/ppm ",vDet[2].Data(),vDet[3].Data()),
+		  user_cut,"prof");
+
+  c1->cd(5);
+  mulc_tree->Draw(Form("(%s/ppm - %s/ppm)/2",vDet[2].Data(),vDet[3].Data()),
+		  user_cut);
+
+  c1->cd(6);  
+  mulc_tree->Draw(Form("(%s/ppm + %s/ppm)/2",vDet[2].Data(),vDet[3].Data()),
+		  user_cut);
+
+  filename = Form("run%s_combo_%s_vs_%s.png",
+		  run_seg.Data(),
+		  vDet[2].Data(),
+		  vDet[3].Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c1->cd();
+  label->Draw("same");
+  c1->SaveAs(output_path+filename);
+  c1->Clear("D");
+
+  // Combo Det and double differences
+
+  TCanvas *c4 = new TCanvas("c4","c4",2400,2400);
+  c4->Divide(2,2);
+  c4->cd(1);
+  mulc_tree->Draw(Form("(%s+%s)/2/ppm ",vDet[0].Data(),vDet[1].Data()),
+		  user_cut);
+  c4->cd(2);
+  mulc_tree->Draw(Form("(%s+%s)/2/ppm ",vDet[2].Data(),vDet[3].Data()),
+		  user_cut);
+  c4->cd(3);
+  mulc_tree->Draw(Form("(%s+%s)/2/ppm:(%s+%s)/2/ppm ",
+		       vDet[0].Data(),vDet[1].Data(),
+		       vDet[2].Data(),vDet[3].Data()),
+		  user_cut);
+
+  c4->cd(4);  
+  mulc_tree->Draw(Form("(%s+%s)/2/ppm-(%s+%s)/2/ppm ",
+		       vDet[0].Data(),vDet[1].Data(),
+		       vDet[2].Data(),vDet[3].Data()),
+		  user_cut);
+
+
+  filename = Form("run%s_combo_%s_dd.png",run_seg.Data(),tag.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c4->cd();
+  label->Draw("same");
+  c4->SaveAs(output_path+filename);
+
+
+  // THe other group of plots
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(1);
+
+  TCanvas *c0 = new TCanvas("c0","c0",2400,1200);
+  c0->Divide(4,2);
+
+  //  ======== vSAM vs AqDS
+  for(int i=0;i<4;i++){
+    c0->cd(i+1);
+    mulc_tree->Draw(Form("%s/ppm:AqDS/ppm",vDet[i].Data())
+		    ,user_cut);
+    c0->cd(i+5);
+    TString hist_name = "hds" +vDet[i];
+    mulc_tree->Draw(Form("%s/ppm:AqDS/ppm>>%s",
+			 vDet[i].Data(),hist_name.Data()),
+		    user_cut,"prof");
+    hfit = (TH2D*)gDirectory->FindObject(hist_name);
+    if(hfit!=NULL){
+      low = hfit->GetMean()-2*hfit->GetRMS();
+      up = hfit->GetMean()+2*hfit->GetRMS();
+      hfit->Fit("pol1","QR","",low,up);
+    }
+  } 
+  filename = Form("run%s_combo_%s_vs_AqDS.png",
+		  run_seg.Data(),
+		  tag.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c0->cd();
+  label->Draw("same");
+  c0->SaveAs(output_path+filename);
+  c0->Clear("D");
+  //  ======== vSAM vs AqUS
+  for(int i=0;i<4;i++){
+
+    c0->cd(i+1);
+    mulc_tree->Draw(Form("%s/ppm:AqUS/ppm",vDet[i].Data())
+		    ,user_cut);
+
+    c0->cd(i+5);
+    
+    TString hist_name = "hus" +vDet[i];
+    mulc_tree->Draw(Form("%s/ppm:AqUS/ppm>>%s",
+			 vDet[i].Data(),hist_name.Data()),
+		    user_cut,"prof");
+    hfit = (TH2D*)gDirectory->FindObject(hist_name);
+    if(hfit!=NULL){
+      low = hfit->GetMean()-2*hfit->GetRMS();
+      up = hfit->GetMean()+2*hfit->GetRMS();
+      hfit->Fit("pol1","QR","",low,up);
+    }
+  } 
+
+  filename = Form("run%s_combo_%s_vs_AqUS.png",
+		  run_seg.Data(),
+		  tag.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c0->cd();
+  label->Draw("same");
+  c0->SaveAs(output_path+filename);
+  
+  // Double difference bewteen detector and BCM 
+  gStyle->SetOptStat(1);
+
+  c4->Clear("D");
+  c4->cd(1);
+  mulc_tree->Draw("AqUS/ppm",user_cut);
+  c4->cd(2);
+  mulc_tree->Draw("AverageLumi/ppm",user_cut);
+  c4->cd(3);
+  mulc_tree->Draw("AverageLumi/ppm:AqUS/ppm",user_cut);
+  c4->cd(4);
+  mulc_tree->Draw("AverageLumi/ppm-AqUS/ppm",user_cut);
+
+  filename = Form("run%s_combo_%s_AqUS_dd.png",
+		  run_seg.Data(),
+		  tag.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c4->cd();
+  label->Draw("same");
+  c4->SaveAs(output_path+filename);
+
+  c4->Clear("D");
+  c4->cd(1);
+  mulc_tree->Draw("AqDS/ppm",user_cut);
+  c4->cd(2);
+  mulc_tree->Draw("AverageLumi/ppm",user_cut);
+  c4->cd(3);
+  mulc_tree->Draw("AverageLumi/ppm:AqDS/ppm",user_cut);
+  c4->cd(4);
+  mulc_tree->Draw("AverageLumi/ppm-AqDS/ppm",user_cut);
+
+  filename = Form("run%s_combo_%s_AqDS_dd.png",
+		  run_seg.Data(),
+		  tag.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c4->cd();
+  label->Draw("same");
+  c4->SaveAs(output_path+filename);
+
+  gSystem->Exec(Form("convert $(ls -rt %s/*combo*.png) %s/run%s_summary_combo_%s.pdf",
+		     output_path.Data(),
+		     output_path.Data(),run_seg.Data(),tag.Data()));
+  gSystem->Exec(Form("rm %s/*combo*.png ",output_path.Data()));
+
+}
+
---
+++ rootMacros/CheckRegNormDetector.C
@@ -0,0 +1,256 @@
+// CheckRegressedDetector.C : 
+//	ROOT script for checking regressed Detector signal from mulc_lrb tree
+//
+// author : Tao Ye <tao.ye@stonybrook.edu>
+// 	 06-2019
+
+
+void CheckRegressedDetector(){
+
+  gStyle->SetStatW(0.3);
+  gStyle->SetStatH(0.4);
+
+  TTree *mul_tree = (TTree*)gROOT->FindObject("mul");
+  TTree *mulc_lrb_tree = (TTree*)gROOT->FindObject("mulc_lrb");
+  mul_tree->AddFriend(mulc_lrb_tree);
+  mul_tree->SetAlias("AqDS","asym_bcm_an_ds3");
+  mul_tree->SetAlias("AqUS","asym_bcm_an_us");
+
+  TCanvas *c1 = new TCanvas("c1","c1",2400,1600);
+  c1->Divide(3,2);
+
+  c1->cd(1);
+  mul_tree->Draw("asym_usl/ppm","ErrorFlag==0");
+  c1->cd(2);
+  mul_tree->Draw("asym_usr/ppm","ErrorFlag==0");
+  c1->cd(3);
+  mul_tree->Draw("asym_usl/ppm:asym_usr/ppm","ErrorFlag==0");
+  c1->cd(4);
+  mul_tree->Draw("(asym_usl/ppm-asym_usr/ppm)/2","ErrorFlag==0");
+  c1->cd(5);
+  mul_tree->Draw("(asym_usl/ppm+asym_usr/ppm)/2","ErrorFlag==0");
+  c1->cd(6);
+  mul_tree->Draw("asym_usl/ppm:asym_usr/ppm",
+		 "ErrorFlag==0","prof");
+    
+  plot_title = Form("run%s_upstreamLR_ratecheck_raw.png",run_seg.Data());
+
+  TText *t1 = new TText(0.0,0.007,plot_title);
+  t1->SetNDC();
+  t1->SetTextSize(0.05);
+  c1->cd();
+  t1->Draw("same");
+  c1->SaveAs(output_path+plot_title);
+  c1->Clear("D");
+  
+
+  // Downstream
+  c1->cd(1);
+  mul_tree->Draw("asym_dsl/ppm","ErrorFlag==0");
+  c1->cd(2);
+  mul_tree->Draw("asym_dsr/ppm","ErrorFlag==0");
+  c1->cd(3);
+  mul_tree->Draw("asym_dsl/ppm:asym_dsr/ppm","ErrorFlag==0");
+  c1->cd(4);
+  mul_tree->Draw("(asym_dsl/ppm-asym_dsr/ppm)/2","ErrorFlag==0");
+  c1->cd(5);
+  mul_tree->Draw("(asym_dsl/ppm+asym_dsr/ppm)/2","ErrorFlag==0");
+  c1->cd(6);
+  mul_tree->Draw("asym_dsl/ppm:asym_dsr/ppm","ErrorFlag==0",
+		 "prof");
+  plot_title = Form("run%s_downstreamLR_ratecheck_raw.png",run_seg.Data());
+
+  t1 = new TText(0.0,0.007,plot_title);
+  t1->SetNDC();
+  t1->SetTextSize(0.05);
+  c1->cd();
+  t1->Draw("same");
+  c1->SaveAs(output_path+plot_title);
+  c1->Clear("D");
+
+
+  // Regression Corrected
+  c1->SetCanvasSize(2400,1200);
+  c1->Clear();
+  c1->Divide(4,2);
+  c1->cd(1);
+  // Upstream
+  TH1 *hfit, *h_buff;
+  Double_t low,up;
+  
+  c1->cd(1);
+  mul_tree->Draw("cor_usl/ppm","ErrorFlag==0");
+  c1->cd(2);
+  mul_tree->Draw("cor_usr/ppm","ErrorFlag==0");
+  c1->cd(3);
+  mul_tree->Draw("cor_usl/ppm:cor_usr/ppm","ErrorFlag==0");
+  c1->cd(4);
+  mul_tree->Draw("cor_usl/ppm:cor_usr/ppm>>hcoruslr","ErrorFlag==0",
+		 "prof");
+  gPad->Update();
+  hfit = (TH2D*)gDirectory->FindObject("hcoruslr");
+  TPaveStats *psus = (TPaveStats*)hfit->FindObject("stats");
+  if(psus!=NULL){
+    psus->SetOptFit(1);
+    psus->SetOptStat(0);
+    psus->SetX1NDC(0.0);
+    psus->SetY1NDC(0.95);
+    psus->SetX2NDC(0.35);
+    psus->SetY2NDC(0.7);
+  }
+  if(hfit!=NULL){
+    low = hfit->GetMean()-2*(hfit->GetRMS());
+    up = hfit->GetMean()+2*(hfit->GetRMS());
+    hfit->Fit("pol1","QR","",low,up);
+  } 
+  TVirtualPad *pad_buff;
+  pad_buff = c1->cd(5);
+  mul_tree->Draw("(cor_usl/ppm-cor_usr/ppm)/2","ErrorFlag==0");
+
+  pad_buff = c1->cd(6);
+  mul_tree->Draw("(cor_usl/ppm-cor_usr/ppm)/2:pattern_number","ErrorFlag==0","COL");
+  TH2F *h2dus_buff = (TH2F*)pad_buff->FindObject("htemp");
+  if(h2dus_buff!=NULL){
+    h2dus_buff->Draw("candlex3");  
+  }
+
+  c1->cd(7);
+  mul_tree->Draw("(cor_usl/ppm+cor_usr/ppm)/2","ErrorFlag==0");
+
+  pad_buff= c1->cd(8);
+  mul_tree->Draw("(cor_usl/ppm+cor_usr/ppm)/2:pattern_number","ErrorFlag==0","COL");
+  TH2F *h2dussum_buff = (TH2F*)pad_buff->FindObject("htemp");
+  if(h2dussum_buff!=NULL)
+    h2dussum_buff->Draw("candlex3");  
+
+  plot_title = Form("run%s_upstreamLR_ratecheck_reg.png",run_seg.Data());
+
+  t1 = new TText(0.0,0.007,plot_title);
+  t1->SetNDC();
+  t1->SetTextSize(0.05);
+  c1->cd();
+  t1->Draw("same");
+  c1->SaveAs(output_path+plot_title);
+  c1->Clear("D");
+
+  // Downstream
+  c1->cd(1);
+  mul_tree->Draw("cor_dsl/ppm","ErrorFlag==0");
+  c1->cd(2);
+  mul_tree->Draw("cor_dsr/ppm","ErrorFlag==0");
+  c1->cd(3);
+  mul_tree->Draw("cor_dsl/ppm:cor_dsr/ppm","ErrorFlag==0");
+  c1->cd(4);
+  mul_tree->Draw("cor_dsl/ppm:cor_dsr/ppm>>hcordslr","ErrorFlag==0",
+		 "prof");
+  gPad->Update();
+  hfit = (TH2D*)gDirectory->FindObject("hcordslr");
+  TPaveStats *psds = (TPaveStats*)hfit->FindObject("stats");
+  if(psds!=NULL){
+    psds->SetOptFit(1);
+    psds->SetOptStat(0);
+    psds->SetX1NDC(0.0);
+    psds->SetY1NDC(0.95);
+    psds->SetX2NDC(0.35);
+    psds->SetY2NDC(0.7);
+  }
+  if(hfit!=NULL){
+    low = hfit->GetMean()-2*hfit->GetRMS();
+    up = hfit->GetMean()+2*hfit->GetRMS();
+    hfit->Fit("pol1","QR","",low,up);
+
+  } 
+
+  c1->cd(5);
+  mul_tree->Draw("(cor_dsl/ppm-cor_dsr/ppm)/2","ErrorFlag==0");
+
+  pad_buff=c1->cd(6);
+  mul_tree->Draw("(cor_dsl/ppm-cor_dsr/ppm)/2:pattern_number","ErrorFlag==0","COL");
+  TH2F *h2dds_buff = (TH2F*)pad_buff->FindObject("htemp");
+  if(h2dds_buff!=NULL)
+    h2dds_buff->Draw("candlex3");  
+
+  c1->cd(7);
+  mul_tree->Draw("(cor_dsl/ppm+cor_dsr/ppm)/2","ErrorFlag==0");
+
+  c1->cd(8);
+  mul_tree->Draw("(cor_dsl/ppm+cor_dsr/ppm)/2:pattern_number","ErrorFlag==0","COL");
+  TH2F *h2ddssum_buff = (TH2F*)pad_buff->FindObject("htemp");
+  if(h2ddssum_buff!=NULL)
+    h2ddssum_buff->Draw("candlex3");  
+    
+  plot_title = Form("run%s_downstreamLR_ratecheck_reg.png",run_seg.Data());
+
+  t1 = new TText(0.0,0.007,plot_title);
+  t1->SetNDC();
+  t1->SetTextSize(0.05);
+  c1->cd();
+  t1->Draw("same");
+  c1->SaveAs(output_path+plot_title);
+  c1->Clear("D");
+
+  //  ======== vMainDet vs AqDS
+  gStyle->SetOptFit(1);
+  gStyle->SetOptStat(0);
+  gStyle->SetStatW(0.3);
+  gStyle->SetStatH(0.25);
+  TCanvas *c0 = new TCanvas("c0","c0",2400,1200);
+  c0->Divide(4,2);
+  TString filename;
+  TText *label;
+
+  TString user_cut = "ErrorFlag==0";
+  for(int i=0;i<4;i++){
+    c0->cd(i+1);
+    mul_tree->Draw(Form("cor_%s/ppm:AqDS/ppm",vMainDet[i])
+		    ,user_cut);
+    c0->cd(i+5);
+    TString hist_name = "hds" +TString(vMainDet[i]);
+    mul_tree->Draw(Form("cor_%s/ppm:AqDS/ppm>>%s",
+			 vMainDet[i],hist_name.Data()),
+		    user_cut,"prof");
+    hfit = (TH2D*)gDirectory->FindObject(hist_name);
+    if(hfit!=NULL){
+      low = hfit->GetMean()-2*hfit->GetRMS();
+      up = hfit->GetMean()+2*hfit->GetRMS();
+      hfit->Fit("pol1","QR","",low,up);
+    }
+  } 
+  filename = Form("run%s_main_vs_AqDS_ratecheck_reg.png",
+		  run_seg.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c0->cd();
+  label->Draw("same");
+  c0->SaveAs(output_path+filename);
+  c0->Clear("D");
+  //  ======== vMainDet vs AqUS
+  for(int i=0;i<4;i++){
+    c0->cd(i+1);
+    mul_tree->Draw(Form("cor_%s/ppm:AqUS/ppm",vMainDet[i])
+		    ,user_cut);
+    c0->cd(i+5);
+    TString hist_name = "hds" +TString(vMainDet[i]);
+    mul_tree->Draw(Form("cor_%s/ppm:AqUS/ppm>>%s",
+			 vMainDet[i],hist_name.Data()),
+		    user_cut,"prof");
+    hfit = (TH2D*)gDirectory->FindObject(hist_name);
+    if(hfit!=NULL){
+      low = hfit->GetMean()-2*hfit->GetRMS();
+      up = hfit->GetMean()+2*hfit->GetRMS();
+      hfit->Fit("pol1","QR","",low,up);
+    }
+  } 
+  filename = Form("run%s_main_vs_AqUS_ratecheck_reg.png",
+		  run_seg.Data());
+  label = new TText(0.0,0.01,filename);     
+  label->SetTextSize(0.03);
+  label->SetNDC();
+  c0->cd();
+  label->Draw("same");
+  c0->SaveAs(output_path+filename);
+  c0->Clear("D");
+
+  gStyle->SetOptStat(1);    
+}
