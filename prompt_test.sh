4914fca3699331a1a94231e7445c5a0f161c2523
Tao Ye <tao.ye@stonybrook.edu>
2019-07-02 09:11:38 -0400

    a testing script

---
+++ prompt_test.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+runnum=$1;
+
+#  Remove the LRB output files if they exist
+shopt -s extglob
+# find split file
+slopefile_list=$(ls -1 ./LRBoutput/blueR$runnum*slope.root);
+shopt -u extglob
+
+for slopefile in $slopefile_list
+do
+    echo "deleting "  $slopefile;
+    rm -f $slopefile;
+done
+
+#JAPAN First Pass
+timenow=$(date +"%Y-%m%d-%H%M");
+
+./qwparity -r $runnum -c prex_cprompt.conf \
+    --event 0:100000 \
+    --rootfile-stem prexCprompt_pass1_ \
+    --QwLog.logfile ./LogFiles/QwLog_run$runnum\_prompt_pass1_$timenow.txt ;
+
+# cp ./japanOutput/summary_$runnum.txt \
+#     ./SummaryText/summary_prompt_pass1_$runnum.txt;
+
+#JAPAN Second Pass
+timenow=$(date +"%Y-%m%d-%H%M");
+
+./qwparity -r $runnum -c prex_cprompt.conf \
+    --event 0:100000 \
+    --rootfile-stem prexCprompt_pass2_ \
+    --QwLog.logfile ./LogFiles/QwLog_run$runnum\_prompt_pass2_$timenow.txt ;
+
+# BeamMod Data Extraction (FIXME)
+# BeamMod Instant Slope Calculation (FIXME)
+# BeamMod Instant correction and summary (FIXME)
+
+# Make Summary Plots/Text and sync to HallA onlineWeb
+./summary.sh $runnum;
+    
